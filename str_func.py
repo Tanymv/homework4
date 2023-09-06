9f4023fef39a841330bcf55f5a8a512ca1c0b616
Tatiana Makarova <tanymv2004@gmail.com>
2023-09-06 19:23:02 +0500

    add .gitignore

--- str_func.py
+++ str_func.py
@@ -1,2 +1,7 @@
 def raising_the_register(str_):
     return str_.upper()
+
+
+def raising_the_first(str_):
+    """функция, которая делает заглавными первые буквы каждого слова в строке"""
+    return str_.title()
